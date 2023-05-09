178960f2847f6526c73eb33f22618bbf8a067f9f
itrofi31 <75306028+itrofi31@users.noreply.github.com>
2023-05-09 11:22:33 +0300

    fix: now if folder chang, copy folder change as well

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,9 +3,19 @@ const path = require('path');
 
 async function copyDirectory(src, dest) {
   try {
+    await fs.rm(dest, { recursive: true });
+
     await fs.mkdir(dest, { recursive: true });
 
     const files = await fs.readdir(src, { withFileTypes: true });
+    // const destFiles = await fs.readdir(dest, { withFileTypes: true });
+    // if (destFiles.length > 0) {
+    //   for (const file of files) {
+    //     console.log(file);
+    //     const destPath = path.join(dest, file.name);
+    //     await fs.unlink(destPath, (err) => {});
+    //   }
+    // }
 
     for (const file of files) {
       const srcPath = path.join(src, file.name);
